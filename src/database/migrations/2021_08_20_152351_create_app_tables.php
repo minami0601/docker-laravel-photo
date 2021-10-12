c1e276bb5536b8e18705e75b56391821e9fd4bb8
minami0601 <minami.love.bb@icloud.com>
2021-10-12 12:57:12 +0900

    出品処理実装

--- src/database/migrations/2021_08_20_152351_create_app_tables.php
+++ src/database/migrations/2021_08_20_152351_create_app_tables.php
@@ -46,10 +46,13 @@ class CreateAppTables extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('seller_id');
-            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('buyer_id')->nullable();
             $table->unsignedBigInteger('secondary_category_id');
             $table->unsignedBigInteger('item_condition_id');
-
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('price');
+            $table->string('state');
             $table->timestamps();
 
             $table->foreign('seller_id')->references('id')->on('users');
